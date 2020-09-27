1ca6b6ac3185b4635663e69822a5031927f4b8a5
Amir <amir@gmail.com>
2020-09-27 11:52:08 +0330

    first commit and only create files

---
+++ database/factories/UploadsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Uploads;
+use Faker\Generator as Faker;
+
+$factory->define(Uploads::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
